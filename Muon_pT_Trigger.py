a20fd734982bfb5f56d2668d22b25620d3acc742
julius-lange <86829869+julius-lange@users.noreply.github.com>
2022-02-07 10:44:49 +0000

    Update Muon_pT_Trigger.py

--- Muon_pT_Trigger.py
+++ Muon_pT_Trigger.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Code to filter out events where no muon has transverse momentum >1.78GeV.
+Code to filter out events where no muon has transverse momentum >1.76GeV.
 Corresponds to L0muon trigger in OneNote.
 """
 
@@ -17,7 +17,7 @@ max_row = 500000 # number of events to read
 
 mu_plus_PT,mu_minus_PT = mu_plus_PT[:max_row],mu_minus_PT[:max_row]
 
-thre = 1780 #MeV, threshold (value used in literature dependendt on LHCb version)
+thre = 1760 #MeV, threshold (value used in literature dependendt on LHCb version)
 loc_mu_plus = (mu_plus_PT > thre) 
 loc_mu_minus = (mu_minus_PT > thre)
 
@@ -28,7 +28,7 @@ accepted_events = []
 
  
 for i in range (len(loc_mu_plus)):
-    if loc_mu_plus[i]==False and loc_mu_plus[i]==False:
+    if loc_mu_plus[i]==False and loc_mu_minus[i]==False:
         failed_events.append(i)
     else:
         accepted_events.append(i)
@@ -36,7 +36,7 @@ for i in range (len(loc_mu_plus)):
                 
 #%%
 # displaying the disregarded events
-print(f"Transverse muon momenta check (the first 100 events); diregarded events; threshold = {thre} MeV")
+print(f"Transverse muon momenta check; diregarded events; threshold = {thre} MeV")
 dict = {'mu_plus_PT' : mu_plus_PT[failed_events],
         'mu_minus_PT' : mu_minus_PT[failed_events]}
 df = pd.DataFrame(dict)
@@ -45,7 +45,7 @@ display(df)
 #%%
 
 # displaying the accepted events
-print(f"Transverse muon momenta check (the first 100 events); accepted events; threshold = {thre} MeV")
+print(f"Transverse muon momenta check; accepted events; threshold = {thre} MeV")
 
 dict = {'mu_plus_PT' : mu_plus_PT[accepted_events],
         'mu_minus_PT' : mu_minus_PT[accepted_events]}
