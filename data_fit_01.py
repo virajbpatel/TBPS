123ea74946a09bfacd32991cc66d218a58b488a1
Kevin <57576354+wzt1799@users.noreply.github.com>
2022-02-16 14:39:45 +0000

    Add files via upload

--- data_fit_01.py
+++ data_fit_01.py
@@ -10,40 +10,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from iminuit import Minuit as minuit
 import module_fit_functions as mfit
+import MODULE_curve_fitting_functions as mcf
 
 
 # Import datasets and plot the histograms
-df = pd.read_pickle('filt_frst_3101.pkl')
+df = pd.read_pickle('filt_frst_802.pkl')
 
-var = ['phi', 'costhetal', 'costhetak', 'q2']
+var = ['phi', 'costhetal', 'costhetak', 'q2']   
+bins = mcf.create_sorted_q2_bins(df)
 
-for i in var:    
-    plt.hist(df[i], bins = 25, density = True, histtype = 'step')
-    plt.title('fit_first_3101')
-    plt.xlabel(i)
-    plt.ylabel('P')
-    plt.show()
+# for i in var:    
+#     plt.hist(df[i], bins = 25, density = True, histtype = 'step')
+#     plt.title('fit_first_3101')
+#     plt.xlabel(i)
+#     plt.ylabel('P')
+#     plt.show()
 
-df_mc = pd.read_pickle('acceptance_mc.pkl')
+df_mc = pd.read_pickle('filt_frst_acc_802.pkl')
+bins_mc = mcf.create_sorted_q2_bins(df_mc)
 
-for i in var:
-    plt.hist(df_mc[i], bins = 25, density = True, histtype = 'step')
-    plt.title('acceptance_mc')
-    plt.xlabel(i)
-    plt.ylabel('P')
-    plt.show()
+# for i in var:
+#     plt.hist(df_mc[i], bins = 25, density = True, histtype = 'step')
+#     plt.title('acceptance_mc')
+#     plt.xlabel(i)
+#     plt.ylabel('P')
+#     plt.show()
 
 #%% overlay the two histograms
-
+b = 0
 i = var[1]
-n, bins, patch = plt.hist(df[i], bins = 25, density = True, histtype = 'step')
-n_mc, bins_mc, patch_mc = plt.hist(df_mc[i], bins = 25, density = True, histtype = 'step')
+n, edge, patch = plt.hist(bins[b][i], bins = 25, density = True, histtype = 'step', label = 'total data')
+n_mc, edge_mc, patch_mc = plt.hist(bins_mc[b][i], bins = 25, density = True, histtype = 'step', label = 'acceptance data')
 
-x_acc, n_acc, params_acc, cov_acc = mfit.accept_fit(n_mc, bins_mc)
+x_acc, n_acc, params_acc, cov_acc = mfit.accept_fit(n_mc, edge_mc)
 
-plt.plot(x_acc, n_acc)
+plt.plot(x_acc, n_acc, label = 'fitted acceptance function')
 plt.xlabel(i)
-plt.ylabel('P')
+plt.ylabel('Prob density')
+plt.legend()
 plt.show()
 
 def acceptance_func(x):
@@ -52,13 +56,17 @@ def acceptance_func(x):
 
 def integrate(x,y):
     da_list = []
+    x = x.to_numpy()
+    y = y.to_numpy()
     for i in range(len(x)-1):
         dx = x[i+1] - x[i]
         avg_y = (y[i+1] + y[i])/2
         da = avg_y*dx
+        if da < 0:
+            da = 0
         da_list.append(da)
     A = np.sum(da_list)
-    return A
+    return abs(A)
 
 def d2gamma_p_d2q2_dcostheta(fl, afb, cos_theta_l):
     """
@@ -89,24 +97,11 @@ def norm_d2gamma_p_d2q2_dcostheta(fl, afb, cos_theta_l):
     return normalised_scalar_array
 #%%
 
-df_ctl = df['costhetal'].sort_values().to_numpy()
-print(df['costhetal'].sort_values())
-
-y_test = d2gamma_p_d2q2_dcostheta(0, 0.07, df_ctl)
-y_test2 = norm_d2gamma_p_d2q2_dcostheta(0, 0.07, df_ctl)
-
-plt.plot(df_ctl, y_test)
-plt.show()
-plt.plot(df_ctl, y_test2)
-
-print(integrate(df_ctl, y_test))
-print(integrate(df_ctl, y_test2))
-
-# showed that norm_d2gamma_p_d2q2_dcostheta is a normalised distributiom in the integral over costhetal
+df_ctl = bins[b][i].sort_values()
 
 
 #%%
-def log_likelihood(fl, afb):
+def log_likelihood(fl, afb, _bin):
     """
     Returns the negative log-likelihood of the pdf defined above
     :param fl: f_l observable
@@ -114,25 +109,23 @@ def log_likelihood(fl, afb):
     :param _bin: number of the bin to fit
     :return:
     """
-    normalised_scalar_array = norm_d2gamma_p_d2q2_dcostheta(fl=fl, afb=afb, cos_theta_l= df_ctl)
+    _bin = bins[int(_bin)]
+    ctl = _bin['costhetal']
+    normalised_scalar_array = d2gamma_p_d2q2_dcostheta(fl=fl, afb=afb, cos_theta_l= ctl)
     return - np.sum(np.log(normalised_scalar_array))
 
 x = np.linspace(-1,1,1000)
-_test_afb = 0.7
-_test_fl = 0.0
-
-# plt.plot(x, [norm_d2gamma_p_d2q2_dcostheta(fl=i, afb, cos_theta_l)for i in x])
-# plt.show()
-# plt.plot(x, [log_likelihood(fl=i, afb = _test_afb, ctl=df_ctl) for i in x])
-
+_test_afb = 0
+_test_fl = 0.2
+_test_bin = b
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 3))
-ax1.plot(x, [log_likelihood(fl=i, afb=_test_afb) for i in x])
+ax1.plot(x, [log_likelihood(fl=i, afb=_test_afb, _bin = _test_bin) for i in x])
 ax1.set_title(r'$A_{FB}$ = ' + str(_test_afb))
 ax1.set_xlabel(r'$F_L$')
 ax1.set_ylabel(r'$-\mathcal{L}$')
 ax1.grid()
-ax2.plot(x, [log_likelihood(fl=_test_fl, afb=i) for i in x])
+ax2.plot(x, [log_likelihood(fl=_test_fl, afb=i, _bin = _test_bin) for i in x])
 ax2.set_title(r'$F_{L}$ = ' + str(_test_fl))
 ax2.set_xlabel(r'$A_{FB}$')
 ax2.set_ylabel(r'$-\mathcal{L}$')
@@ -147,12 +140,12 @@ bin_results_to_check = None
 
 log_likelihood.errordef = minuit.LIKELIHOOD
 decimal_places = 3
-starting_point = [-0.1,0.0]
+starting_point = [0.6,-0.2]
 fls, fl_errs = [], []
 afbs, afb_errs = [], []
-m = minuit(log_likelihood, fl=starting_point[0], afb=starting_point[1])
-# m.fixed['ctl'] = True  # fixing the bin number as we don't want to optimize it
-m.limits=((-1.0, 1.0), (-1.0, 1.0))
+m = minuit(log_likelihood, fl=starting_point[0], afb=starting_point[1], _bin = b)
+m.fixed['_bin'] = True  # fixing the bin number as we don't want to optimize it
+m.limits=((-1.0, 1.0), (-1.0, 1.0), None)
 m.migrad()
 m.hesse()
 bin_results_to_check = m
@@ -177,9 +170,9 @@ plt.show()
 
 #%%
 
-plt.hist(df[var[1]], bins = 25, density = True, histtype = 'step')
+plt.hist(df_ctl, bins = 25, density = True, histtype = 'step')
 plt.plot(df_ctl, norm_d2gamma_p_d2q2_dcostheta(fls[0], afbs[0], df_ctl))
 plt.title('fit_first_3101')
-plt.xlabel(var[1])
+plt.xlabel(i)
 plt.ylabel('P')
 plt.show()
