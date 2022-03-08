621579779e0bc9f04136179545ae6a32e77b4c18
HYC1102 <94969475+HYC1102@users.noreply.github.com>
2022-03-08 19:00:31 +0000

    a_0 variable included

--- q2 bins fits (with Combinatorial Background).py
+++ q2 bins fits (with Combinatorial Background).py
@@ -47,8 +47,8 @@ def integrate(x,y):
     A = np.sum(da_list)
     return abs(A)
 
-def chebyshev(x, a_1, a_2):
-    return 1 + a_1 * x + a_2 * (2 * x ** 2 -1)
+def chebyshev(x, a_0, a_1, a_2):
+    return a_0 + a_1 * x + a_2 * (2 * x ** 2 -1)
 
 def d2gamma_p_d2q2_dcosthetal(fl, afb, cos_theta_l):
     """
@@ -63,7 +63,7 @@ def d2gamma_p_d2q2_dcosthetal(fl, afb, cos_theta_l):
     scalar_array = 3/8 * (3/2 - 1/2 * fl + 1/2 * c2tl * (1 - 3 * fl) + 8/3 * afb * ctl) * acceptance_func(ctl)
     return scalar_array
 
-def norm_d2gamma_p_d2q2_dcosthetal(fl, afb, a_1, a_2, cos_theta_l):
+def norm_d2gamma_p_d2q2_dcosthetal(fl, afb, a_0, a_1, a_2, cos_theta_l):
     """
     Returns the pdf defined above
     :param fl: f_l observable
@@ -71,13 +71,13 @@ def norm_d2gamma_p_d2q2_dcosthetal(fl, afb, a_1, a_2, cos_theta_l):
     :param cos_theta_l: cos(theta_l)
     :return:
     """
-    scalar_array = d2gamma_p_d2q2_dcosthetal(fl, afb, cos_theta_l) + chebyshev(cos_theta_l, a_1, a_2)
+    scalar_array = d2gamma_p_d2q2_dcosthetal(fl, afb, cos_theta_l) + chebyshev(cos_theta_l, a_0, a_1, a_2)
     cos_theta_l_acc = np.linspace(-1,1,10000)
-    norm_factor = integrate(cos_theta_l_acc, d2gamma_p_d2q2_dcosthetal(fl=fl, afb=afb, cos_theta_l=cos_theta_l_acc) + chebyshev(cos_theta_l_acc, a_1, a_2))
+    norm_factor = integrate(cos_theta_l_acc, d2gamma_p_d2q2_dcosthetal(fl=fl, afb=afb, cos_theta_l=cos_theta_l_acc) + chebyshev(cos_theta_l_acc, a_0, a_1, a_2))
     normalised_scalar_array = scalar_array /norm_factor  
     return normalised_scalar_array
 
-def plotting_func(fl, afb, a_1, a_2, cos_theta_l):
+def plotting_func(fl, afb, a_0, a_1, a_2, cos_theta_l):
     """
     Returns the pdf defined above
     :param fl: f_l observable
@@ -86,14 +86,14 @@ def plotting_func(fl, afb, a_1, a_2, cos_theta_l):
     :return:
     """
     signal = d2gamma_p_d2q2_dcosthetal(fl, afb, cos_theta_l)
-    background = chebyshev(cos_theta_l, a_1, a_2)
+    background = chebyshev(cos_theta_l, a_0, a_1, a_2)
     cos_theta_l_acc = np.linspace(-1,1,10000)
-    norm_factor = integrate(cos_theta_l_acc, d2gamma_p_d2q2_dcosthetal(fl=fl, afb=afb, cos_theta_l=cos_theta_l_acc) + chebyshev(cos_theta_l_acc, a_1, a_2))
+    norm_factor = integrate(cos_theta_l_acc, d2gamma_p_d2q2_dcosthetal(fl=fl, afb=afb, cos_theta_l=cos_theta_l_acc) + chebyshev(cos_theta_l_acc, a_0, a_1, a_2))
     norm_signal = signal / norm_factor  
     norm_background = background / norm_factor 
     return norm_signal, norm_background
 
-def log_likelihood_ctl(fl, afb, a_1, a_2, _bin):
+def log_likelihood_ctl(fl, afb, a_0, a_1, a_2, _bin):
     """
     Returns the negative log-likelihood of the pdf defined above
     :param fl: f_l observable
@@ -103,7 +103,7 @@ def log_likelihood_ctl(fl, afb, a_1, a_2, _bin):
     """
     _bin = bins[int(_bin)]
     ctl = _bin['costhetal']
-    normalised_scalar_array = norm_d2gamma_p_d2q2_dcosthetal(fl=fl, afb=afb, a_1 = a_1, a_2 = a_2, cos_theta_l= ctl)
+    normalised_scalar_array = norm_d2gamma_p_d2q2_dcosthetal(fl=fl, afb=afb, a_0 = a_0, a_1 = a_1, a_2 = a_2, cos_theta_l= ctl)
     return - np.sum(np.log(normalised_scalar_array))
 
 
@@ -131,29 +131,37 @@ for b in range(10):
     starting_point = [0,0]
     fls, fl_errs = [], []
     afbs, afb_errs = [], []
+    a_0s, a_0_errs = [], []
     a_1s, a_1_errs = [], []
     a_2s, a_2_errs = [], []
-    m = minuit(log_likelihood_ctl, fl=starting_point[0], afb=starting_point[1], a_1 = 0, a_2 = 0, _bin = b)
+    m = minuit(log_likelihood_ctl, fl=starting_point[0], afb=starting_point[1], a_0 = 0, a_1 = 0, a_2 = 0, _bin = b)
     m.fixed['_bin'] = True  # fixing the bin number as we don't want to optimize it
-    m.limits=((-1.0, 1.0), (-1.0, 1.0), (-0.2, 0.2), (-0.2, 0.2), None)
+    """
+    Change limits here to allow a_0, a_1 and a_2 to freely vary. At the moment
+    dont seems to have found good limits which allows the fit to converge. 
+    
+    """
+    m.limits=((-1.0, 1.0), (-1.0, 1.0), (0,0), (0, 0), (0, 0), None) 
     m.migrad()
     m.hesse()
     bin_results_to_check = m
     fls.append(m.values[0])
     afbs.append(m.values[1])
-    a_1s.append(m.values[2])
-    a_2s.append(m.values[3])
+    a_0s.append(m.values[2])
+    a_1s.append(m.values[3])
+    a_2s.append(m.values[4])
     fl_errs.append(m.errors[0])
     afb_errs.append(m.errors[1])
-    a_1_errs.append(m.errors[2])
-    a_2_errs.append(m.errors[3])
+    a_0_errs.append(m.errors[2])
+    a_1_errs.append(m.errors[3])
+    a_2_errs.append(m.errors[4])
     print(f"Bin {b}: Fl = {np.round(fls, decimal_places)} pm {np.round(fl_errs, decimal_places)},", 
           f" Afb = {np.round(afbs, decimal_places)} pm {np.round(afb_errs, decimal_places)}. Function minimum considered valid: {m.fmin.is_valid}")
     
     h, e, ppp = plt.hist(bins[b][i].sort_values(), bins = bin_width, density = True, histtype = 'step', color = 'blue')
-    norm_signal, norm_background= plotting_func(fls[0], afbs[0], a_1s[0], a_2s[0], bins[b][i].sort_values())
+    norm_signal, norm_background= plotting_func(fls[0], afbs[0], a_0s[0], a_1s[0], a_2s[0], bins[b][i].sort_values())
     plt.errorbar((e[:-1] + 0.5*e[1] - 0.5*e[0]), h, yerr = np.sqrt(h * len(bins[b][i]))/len(bins[b][i]), fmt = '.', color = 'blue')
-    plt.plot(bins[b][i].sort_values(), norm_d2gamma_p_d2q2_dcosthetal(fls[0], afbs[0],a_1s[0], a_2s[0], bins[b][i].sort_values()), label = "Total fit")
+    plt.plot(bins[b][i].sort_values(), norm_d2gamma_p_d2q2_dcosthetal(fls[0], afbs[0],a_0s[0], a_1s[0], a_2s[0], bins[b][i].sort_values()), label = "Total fit")
     plt.plot(bins[b][i].sort_values(), norm_signal, label = f'fl =  {np.round(fls[0], decimal_places)} pm {np.round(fl_errs[0], decimal_places)} Afb = {np.round(afbs[0], decimal_places)} pm {np.round(afb_errs[0], decimal_places)}')
     plt.plot(bins[b][i].sort_values(), norm_background, label = "Background fit")
     plt.title(f'{data_files[0]} bin {b}')
